5f02c08381bf448d1167ca17d063c10f4c661246
Imanuel Costigan <i.costigan@me.com>
2014-09-19 17:11:20 +1000

    Starting ICAP code (#3)

---
+++ R/icap.R
@@ -0,0 +1,5 @@
+download_icap_zip <- function (entity = 'igdl') {
+  igdl <- 'http://www2.icap.com/sef/marketdata/igdl/ICAPSEFMarketDataReport.zip'
+  icus <- 'http://www2.icap.com/sef/marketdata/ussef/ICAPSEFMarketDataReport.zip'
+  download.file()
+}
