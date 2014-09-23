8517dea28e5480bb415543765472957b54adaea9
Imanuel Costigan <i.costigan@me.com>
2014-09-23 16:38:10 +1000

    Updated ICAP debug script

--- debug/debug_icap.R
+++ debug/debug_icap.R
@@ -3,11 +3,15 @@ require (stringr)
 require (lubridate)
 require (dplyr)
 require (ggplot2)
+require (stringr)
 # Get ICAP data
 get_icap_data(ymd(20140731))
 # Find ICAP files in temp directory
 tempdir <- tempdir()
 files <- list.files(file.path(tempdir, c('icus', 'igdl')), full.names = TRUE)
+sef_labels <- unlist(str_extract(files, '((igdl)|(icus))'))
+date_labels <- unlist(str_extract(files, '[[:digit:]]{8}'))
+file_labels <- paste0(sef, date)
 headers <- list()
 i <- 1
 # Get headers
@@ -15,7 +19,7 @@ for (file in files) {
   headers[[i]] <- scan(file, what = 'string', nlines = 1, sep = ',')
   i <- i + 1
 }
-names(headers) <- str_replace(files, tempdir, '')
+names(headers) <- file_labels
 # Summarise header field counts
 field_counts <- sapply(headers, NROW)
 field_counts <- tbl_df(data.frame(
@@ -33,3 +37,27 @@ headers[str_detect(names(headers), '(icus)*(20131029)+')]
 headers[str_detect(names(headers), '(icus)*(20131216)+')]
 # And then during 2014
 headers[str_detect(names(headers), '(icus)*(20140731)+')]
+
+
+regexps <- c('date|Date|DATE', # date
+  'asset|Asset|ASSET', # assetclass
+  'inst|Inst|INST', # security
+  '((t|T)(rade|RADE))+(\\s)*(V(ol|OL)(ume|UME)*)\\s*(?!\\(*USD\\)*)', # totalvolume
+  '((t|T)(rade|RADE))+(\\s)*(V(ol|OL)(ume|UME)*)\\s*\\(*USD\\)*', #totalvolumeusd
+  'OPEN|Open|open', #priceopen
+  'CLOSE|Close|close', #priceclose
+  'LOW|Low|low', #pricelow
+  'HIGH|High|high' #pricehigh
+  )
+for (regexp in regexps)
+{
+  i <- 0
+  for (header in headers)
+  {
+    flag <- any(str_detect(header, perl(regexp)))
+#     if (!flag) print(header)
+    i <- i + flag
+  }
+  print(paste0(regexp, ': ', i, ' of ', NROW(headers)))
+}
+
