2625a451f628296628a49e278c36e9d7bc0eb87b
Imanuel Costigan <i.costigan@me.com>
2015-05-03 20:04:54 +1000

    Fix test cases

--- tests/testthat/test-ddr.R
+++ tests/testthat/test-ddr.R
@@ -2,11 +2,11 @@ context("DDR")
 library("lubridate")
 
 test_that('DDR URL scheme still valid', {
-  for (asset_class in ASSET_CLASSES) {
-    expect_is(httr::url_ok(ddr_url(ymd(20150430), asset_class)))
+  for (asset_class in names(DDR_ASSET_CLASSES)) {
+    expect_true(httr::url_ok(ddr_url(ymd(20150430), asset_class)))
   }
 })
 
 test_that("DDR zip can be downloaded", {
-  expect_equal(download_ddr_zip(ymd(20150430), "IR"), 0)
+  suppressMessages(expect_equal(download_ddr_zip(ymd(20150430), "IR"), 0))
 })
