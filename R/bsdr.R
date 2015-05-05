0d602d6e614c701f4f692516fd7b8ba0e41045de
Imanuel Costigan <i.costigan@me.com>
2015-05-05 21:03:11 +1000

    Completed coding of BSDR; docs need to be written (#7)

--- R/bsdr.R
+++ R/bsdr.R
@@ -1,10 +1,66 @@
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c("exec_timestamp", "effective_date", "end_date",
+    "price_notation", "additional_price_notation", "notional_currency_amount_1",
+    "option_strike_price", "option_premium", "submission_timestamp",
+    "publication_timestamp", "action", "bespoke_swap", "taxonomy",
+    "price_notation_type", "notional_currency_1", "block_trade", "cleared",
+    "end_user_excpetion"))
+}
+
+#' Get Bloomberg SEF data
+#'
+#' The Bloomberg Swap Execution Facility (SEF) offers customers the ability to
+#' execute derivative instruments across a number of different asset classes. It
+#' is required to make publicly available price, trading volume and other trading
+#' data. It publishes this data on its website. I have reverse engineered the
+#' JavaScript libraries used by its website to call the Bloomberg Application
+#' Service using \code{POST} requests to a target URL. The \code{POST} calls
+#' are done by asset class. This function iterates across all asset classes
+#' (credit, equities, foreign exchange, rates and commodities).
+#'
+#' @param date the date for which data is required as Date or DateTime
+#' object. Only the year, month and day elements of the object are used. Must
+#' be of length one.
+#' @param asset_class the asset class for which you would like to download
+#' trade data. Valid inputs are \code{"CR"} (credit), \code{"IR"} (rates),
+#' \code{"EQ"} (equities), \code{"FX"} (foreign exchange), \code{"CO"}
+#' (commodities). Can be a vector of these. Defaults to \code{NULL} which
+#' corresponds to all asset classes.
+#' @return a data frame containing the requested data, or an empty data frame
+#' if data is unavailable
+#' @importFrom dplyr %>%
+#' @references
+#' \href{http://data.bloombergsef.com}{Bloomberg SEF data}
+#' @examples
+#' library (lubridate)
+#' # All asset classes
+#' get_bsef_data(ymd(20140528))
+#' # Only IR and FX asset classes
+#' get_bsef_data(ymd(20140528), c("IR", "FX"))
+#' @export
+
+get_bsdr_data <- function (date, asset_class = NULL) {
+  valid_asset_classes <- c('CR', 'EQ', 'FX', 'IR', 'CO')
+  if (is.null(asset_class)) {
+    asset_class <- valid_asset_classes
+  }
+  assertthat::assert_that(all(asset_class %in% valid_asset_classes),
+    lubridate::is.instant(date), length(date) == 1)
+  download_bsdr_data(date, asset_class) %>% format_bsdr_data()
+}
+
+download_bsdr_data <- function (date, asset_class) {
+  dplyr::bind_rows(Map(download_bsdr_data_single, date, asset_class))
+}
+
 # Reverse engineering API:
 # http://www.bloombergsdr.com/assets/js/search.js
 download_bsdr_data_single <- function (date_range, asset_class, currency = NULL,
   notional_range = NULL) {
   # If only one date is specified for date_range, then add one day to this to
   # define the end date (i.e. we want date range starting from the date
-  # specified)
+  # specified). This will be only allowable behaviour for time being as this
+  # will ensure consistency with other data sources and minimise API hammering.
   if (length(date_range) == 1) {
     date_range <- date_range + lubridate::days(0:1)
   }
@@ -58,11 +114,33 @@ download_bsdr_data_single <- function (date_range, asset_class, currency = NULL,
 }
 
 bsdr_url <- function () {
- "http://www.bloombergsdr.com/bas/bsdrweb"
+  "http://www.bloombergsdr.com/bas/bsdrweb"
 }
 
 # Header isn't specified in search.js. However, added for sake of consistency
 # with BSEF spec.
 bsdr_header <- function (version = 1.3) {
   httr::add_headers('bas-version' = version)
+}
+
+# Using http://www.bloombergsdr.com/api
+# http://www.bloombergsdr.com/assets/img/BSDR%20API%20for%20Slice%20Files.pdf
+#' @importFrom dplyr %>%
+format_bsdr_data <- function (data) {
+  # Select fields that are presented on the BSDR web search interface
+  data %>%
+    select(
+      action, exec_timestamp, bespoke_swap, taxonomy,
+      price_notation_type, price_notation, notional_currency_1,
+      notional_currency_amount_1, effective_date, end_date, block_trade, cleared,
+      end_user_excpetion, option_strike_price, option_premium) %>%
+    mutate(
+      exec_timestamp = lubridate::fast_strptime(exec_timestamp,
+        "%Y-%m-%dT%H:%M:%OS%z"),
+      effective_date = lubridate::fast_strptime(effective_date, "%m/%d/%Y"),
+      end_date = lubridate::fast_strptime(end_date, "%m/%d/%Y"),
+      price_notation = as.numeric(price_notation),
+      notional_currency_amount_1 = as.numeric(notional_currency_amount_1),
+      option_strike_price = as.numeric(option_strike_price),
+      option_premium = as.numeric(option_premium))
 }
