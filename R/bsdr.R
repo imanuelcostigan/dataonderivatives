dab77539269a7cbb71a507c196455bb0e83a9cfb
Imanuel Costigan <i.costigan@me.com>
2015-05-05 17:14:20 +1000

    Update message for download_bsdr*

--- R/bsdr.R
+++ R/bsdr.R
@@ -1,20 +1,30 @@
-
+# Reverse engineering API:
+# http://www.bloombergsdr.com/assets/js/search.js
 download_bsdr_data_single <- function (from, to, asset_class, currency = NULL,
   notional_range = NULL) {
-  message('Downloading and reading BSDR data for the ', asset_class,
+  msg <- paste0('Downloading and reading BSDR data for the ', asset_class,
     ' asset class for the period from ', format(from, '%d-%b-%Y'), " to ",
-    format(to, '%d-%b-%Y'), '...')
+    format(to, '%d-%b-%Y'))
+  if (!is.null(currency)) {
+    msg <- paste0(msg, " for currency ", currency)
+  }
+  if (!is.null(notional_range)) {
+    msg <- paste0(msg, " with notional range in ", notional_range[1], " to ",
+      notional_range[2])
+  }
+  msg <- paste0(msg, '...')
+  message(msg)
   start_date <- paste0(format(from, '%Y-%m-%d'), 'T00:00:00.000000Z')
   end_date <- paste0(format(to, '%Y-%m-%d'), 'T00:00:00.000000Z')
   # Build POST body
-  # From http://www.bloombergsdr.com/assets/js/search.js
   body <- list(
     data_type = "FULL",
     asset_class = asset_class,
     datetime_low = start_date,
     datetime_high = end_date)
-  if (!is.null(currency))
+  if (!is.null(currency)) {
     body <- c(body, currency = currency)
+  }
   if (!is.null(notional_range)) {
     body <- c(body,
       notional_low = notional_range[1], notional_high = notional_range[2])
