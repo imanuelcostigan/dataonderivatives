cad9ef4f5ccf59650790bb45d803cf7741b997ea
Imanuel Costigan <i.costigan@me.com>
2015-05-07 21:03:50 +1000

    BSEF test case updated for API change

--- tests/testthat/test_bsef.R
+++ tests/testthat/test_bsef.R
@@ -1,6 +1,6 @@
 context("BSEF")
 test_that('BSEF API accesible', {
-  empty_data <- download_bsef_data_single('IR', lubridate::ymd(20140918))
+  empty_data <- download_bsef_data_single(lubridate::ymd(20140918), 'IR')
   expect_false(identical(empty_data, dplyr::data_frame()))
   empty_data <- get_bsef_data(lubridate::ymd(20140920))
   expect_true(identical(empty_data, dplyr::data_frame()))
