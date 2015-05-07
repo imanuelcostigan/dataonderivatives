cbb81012727d94fcb591b45753ecddffcc4f0a5e
Imanuel Costigan <i.costigan@me.com>
2015-05-07 21:04:05 +1000

    Added BSDR test cases (#7)

---
+++ tests/testthat/test-bsdr.R
@@ -0,0 +1,12 @@
+context("BSDR")
+test_that('BSDR API accesible', {
+  library(lubridate)
+  df1 <- download_bsdr_data_single(ymd(20150505), "CO")
+  expect_is(df1, "data.frame")
+  df2 <- download_bsdr_data_single(ymd(20150504, 20150506), "CR")
+  expect_is(df2, "data.frame")
+  expect_true(nrow(df1 <= df2))
+  df3 <- download_bsdr_data_single(ymd(20150505), c("CO", "CR"))
+  expect_is(df3, "data.frame")
+  expect_true(nrow(df1) <= nrow(df3))
+})
