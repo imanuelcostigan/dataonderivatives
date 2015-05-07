56e961674aa0c455e21548aee1b498039e6bb4b6
Imanuel Costigan <i.costigan@me.com>
2015-05-07 21:04:47 +1000

    Fixed test case

--- tests/testthat/test-bsdr.R
+++ tests/testthat/test-bsdr.R
@@ -5,7 +5,7 @@ test_that('BSDR API accesible', {
   expect_is(df1, "data.frame")
   df2 <- download_bsdr_data_single(ymd(20150504, 20150506), "CR")
   expect_is(df2, "data.frame")
-  expect_true(nrow(df1 <= df2))
+  expect_true(nrow(df1) <= nrow(df2))
   df3 <- download_bsdr_data_single(ymd(20150505), c("CO", "CR"))
   expect_is(df3, "data.frame")
   expect_true(nrow(df1) <= nrow(df3))
