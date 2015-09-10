c83012e4784324a1da2f83744e7c435aa3cb04ec
Imanuel Costigan <i.costigan@me.com>
2015-09-10 12:04:25 +1000

    Delete other ICAP files

--- tests/testthat/test_icap.R
+++
@@ -1,5 +0,0 @@
-# context("ICAP")
-# test_that('ICAP files available', {
-#   expect_equal(downloader::download(url = url_icap_zip(TRUE), destfile = tempfile(), quiet = TRUE), 0L)
-#   expect_equal(downloader::download(url = url_icap_zip(FALSE), destfile = tempfile(), quiet = TRUE), 0L)
-# })
